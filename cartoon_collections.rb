a0fcc44df517f0891dfff611118e11e197ac58bb
Maria Yimei Shao <maria.yimei.shao@gmail.com>
2019-08-30 04:18:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ def summon_captain_planet(array)
 end 
 
 def long_planeteer_calls(calls)
- calls.any? {call.length
+ calls.any? {|call| call.length > 4}
 end
 
 def find_the_cheese# code an argument here
