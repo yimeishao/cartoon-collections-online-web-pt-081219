d4bcdd70819e89618d563e5f3eb4a39379e724fb
Maria Yimei Shao <maria.yimei.shao@gmail.com>
2019-08-30 04:16:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,8 @@ def summon_captain_planet(array)
   array.collect {|x| x.capitalize + "!"}
 end 
 
-def long_planeteer_calls(array)
-  array.each do |call| 
+def long_planeteer_calls(calls)
+  calls.each do |call| 
     if call.length > 4 
       return true 
     end
