4baecc99c7bcbe9e499027553788992586dc67da
Maria Yimei Shao <maria.yimei.shao@gmail.com>
2019-08-30 04:19:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,6 @@ def long_planeteer_calls(calls)
  calls.any? {|call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
