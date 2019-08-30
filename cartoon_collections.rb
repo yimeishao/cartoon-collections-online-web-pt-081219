8d13c14c45c6da5eab3f770519cc47075ac06198
Maria Yimei Shao <maria.yimei.shao@gmail.com>
2019-08-30 04:12:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(array)
 end 
 
 def summon_captain_planet(array)
-  array.collect {|x| x.capitalize + !}
+  array.collect {|x| x.capitalize + "!"}
 end 
 
 def long_planeteer_calls# code an argument here
